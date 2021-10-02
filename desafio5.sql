f4309804b5364c24efd50d7d7171d61e374123a2
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-10-02 03:37:04 -0300

    requisito 5 feito

---
+++ desafio5.sql
@@ -0,0 +1,14 @@
+CREATE VIEW top_2_hits_do_momento AS
+    SELECT 
+        (SELECT 
+                nome
+            FROM
+                musicas AS m
+            WHERE
+                m.id = ho.musica_id) AS cancao,
+        COUNT(*) AS reproducoes
+    FROM
+        historico_ouvidas AS ho
+    GROUP BY cancao
+    ORDER BY reproducoes DESC , cancao ASC
+    LIMIT 2;
