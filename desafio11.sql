fb76929b6cf459973614224c23c1e7b6af2e8047
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-10-05 12:17:06 -0300

    req11 climate

--- desafio11.sql
+++ desafio11.sql
@@ -1,13 +1,13 @@
 CREATE VIEW cancoes_premium AS
-    SELECT 
-        m.nome AS nome, COUNT(*) AS reproducoes
-    FROM
-        usuarios AS u
-            INNER JOIN
-        historico_ouvidas AS ho ON u.id = ho.usuario_id
-            INNER JOIN
-        musicas AS m ON m.id = ho.musica_id
-    WHERE
-        plano_id IN (2 , 3)
-    GROUP BY m.nome
-    ORDER BY `nome` ASC;
+SELECT 
+m.nome AS nome, COUNT(*) AS reproducoes
+FROM
+usuarios AS u
+INNER JOIN
+historico_ouvidas AS ho ON u.id = ho.usuario_id
+INNER JOIN
+musicas AS m ON m.id = ho.musica_id
+WHERE
+plano_id IN (2 , 3)
+GROUP BY m.nome
+ORDER BY `nome` ASC;
